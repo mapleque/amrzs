8131fd99e34342ebd53339e2c931e3820dc880f8
mapleque <mapleque@163.com>
2016-09-27 13:24:33 +0800

    create

---
+++ include.php
@@ -0,0 +1,9 @@
+<?php
+
+ignore_user_abort(true);
+
+require dirname(__FILE__) . '/class_loader.php';
+require dirname(__FILE__) . '/base.php';
+require dirname(__FILE__) . '/debug.php';
+
+ClassLoader::init();
