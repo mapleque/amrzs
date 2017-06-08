68133f8741580797c9b75f309ea0bce170b248cc
杨阳 <yangyang@licaigc.com>
2017-06-08 17:31:36 +0800

    remove db component

--- core/db.php
+++ core/db.php
@@ -20,76 +20,9 @@ class DB
         return self::getDB()->select($query, $bind);
     }
 
-    /**
-     * 统一format SQL，业务端只用关注数据
-     * @param $tableName 更新表名
-     * @param $setParams 更新数据
-     * @param $whereCondition 更新条件
-     */
-    public static function componentSelect($tableName,$fileds,$whereCondition)
-    {
-        $whereSQL = [];
-        $params = array_values($whereCondition);
-        foreach ($whereCondition as $key => $value){
-            $whereSQL[] = $key."=?";
-        }
-        $whereSQL = implode(" and ",$whereSQL);
-
-        $query = sprintf("SELECT %s FROM %s WHERE %s",$fileds,$tableName,$whereSQL);
-        return self::select($query,$params);
-    }
-
-    public static function insert($query, $bind = null)
-    {
-        return self::getDB()->insert($query, $bind);
-    }
-
-    /**
-     * 统一format SQL，业务端只用关注数据
-     * @param $tableName 更新表名
-     * @param $params 更新数据
-     * @return mixed
-     */
-    public static function componentInsert($tableName, $params)
-    {
-        $query = sprintf("INSERT INTO TABLE %s (%s) VALUES (%s)",
-            $tableName, implode(",",array_keys($params)) , implode(",",array_fill(0,$params.length,"?"))
-        );
-        return self::update($query,$params);
-    }
-
     public static function update($query, $bind = null)
     {
         return self::getDB()->exec($query, $bind);
-    }
-
-    /**
-     * 统一format SQL，业务端只用关注数据
-     * @param string $tableName 更新表名
-     * @param array $setParams 更新数据
-     * @param array $whereCondition 更新条件
-     * @return mixed
-     */
-    public static function componentUpdate($tableName, $setParams, $whereCondition)
-    {
-        $setSQL = "";
-        $whereSQL = [];
-        $params = [];
-        foreach ($setParams as $key => $value){
-            if($value == ""){
-                continue;
-            }
-            $setSQL .= $key."=?,";
-            $params[] = $value;
-        }
-        $setSQL = trim($setSQL,",");
-        foreach ($whereCondition as $key => $value){
-            $whereSQL[] = $key."=?";
-            $params[] = $value;
-        }
-        $whereSQL = implode(" and ",$whereSQL);
-        $query = sprintf("UPDATE %s SET %s WHERE %s",$tableName,$setSQL,$whereSQL);
-        return self::update($query,$params);
     }
 
     public static function exec($query, $bind = null)
